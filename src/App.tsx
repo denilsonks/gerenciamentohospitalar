911b648ecd94e33bdaaf49fbf996ff08d668092c
Denilson Slebodzinski <k2sdigital25@gmail.com>
2025-11-27 20:58:23 -0300

    Modificado sistema de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { theme } from './theme';
-import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Dashboard />
     </ThemeProvider>
   );
 }
